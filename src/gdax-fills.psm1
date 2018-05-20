edd4e86a0a735ba703372a23493f8262982efbe3
Niels Verbaken <nielsverbaken@outlook.com>
2018-05-20 20:16:03 +0200

    fix Fills endpoint is now marked as Private

--- src/gdax-fills.psm1
+++ src/gdax-fills.psm1
@@ -5,25 +5,60 @@
 $Script:base = '/fills'
 
 
-##
-# Get a list of recent Fills
-#  filter by Product ID
-#  optionaly Before order id to get only newer fills
+<#
+ .Synopsis
+  Gets a list of recent Fills.
+
+ .Description
+  Calls the GDAX API Private endpoint to fetch a list of recent Fills.
+
+ .Parameter ProductID
+  A ProductID to filter the list of Fills
+  
+ .Parameter OrderID
+  An OrderID to filter the list of Fills
+
+ .Parameter Before
+  A TradeID to filter out all Fills from before that trade
+#>
 Function Get-Fills {
     param(
-        [Parameter(Mandatory=$true)]
-        $Product,
+        [Parameter(Mandatory=$false)]
+        $ProductID,
+        [Parameter(Mandatory=$false)]
+        $OrderID,
         [Parameter(Mandatory=$false)]
         $Before
     )
 
-    $endpoint = $Script:base + "?product_id=" + $Product
+    # set the endpoint
+    $endpoint = $Script:base
 
-    if( $Before ){
-        $endpoint += "&order_id=" + $Before
+    # set the concatinator
+    $c = '?'
+
+    if( $MyInvocation.BoundParameters.ContainsKey('ProductID') ){
+        # add product_id param
+        $endpoint += $c + "product_id=" + $ProductID
+
+        # change concatinator
+        $c = '&'
     }
 
-    return Invoke-GDAXEndpoint -Method Get -Path $endpoint
+    if( $MyInvocation.BoundParameters.ContainsKey('OrderID') ){
+        # add order_id param
+        $endpoint += $c + "order_id=" + $OrderID
+
+        # change concatinator
+        $c = '&'
+    }
+
+    if( $MyInvocation.BoundParameters.ContainsKey('Before') ){
+        # add the trade_id param
+        $endpoint += $c + "trade_id=" + $Before
+    }
+
+    return Invoke-GDAXEndpoint -Method Get -Path $endpoint -Private
 }
 
 
